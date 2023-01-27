ecc2e2a891f57c1e77bb484fda1029a55bd86a2c
Dominik <dominikgos3@gmail.com>
2023-01-27 17:55:29 +0100

    add custom ckeditor in thread form

--- vue/src/components/thread/create-thread-form.vue
+++ vue/src/components/thread/create-thread-form.vue
@@ -1,21 +1,54 @@
 <template>
-<form action="">
-  <div class="mb-3">
-    <label for="exampleFormControlInput1" class="form-label">Email addressxXXX</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-  </div>
-  <div class="mb-3">
-    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
-  <div class="mb-3">
-    <button type="submit" class="btn btn-primary">Send</button>
-  </div>
-</form>
+  <form action="">
+    <h3>Create thread.</h3>
+    <div class="mb-3">
+      <label for="title" class="form-label">
+        Title
+      </label>
+      <input
+        v-model="title"
+        type="text"
+        class="form-control"
+        id="title"
+      />
+    </div>
+    <div class="mb-3">
+      <label for="description" class="form-label">
+        Description
+      </label>
+      <ckeditor
+        @input="log()"
+        id="description"
+        :editor="editor"
+        v-model="editorData"
+        :config="editorConfig"
+      ></ckeditor>
+    </div>
+    <div class="mb-3">
+      <button type="submit" class="btn btn-primary">Send</button>
+    </div>
+  </form>
 </template>
 
 <script>
+import Editor from 'ckeditor5-custom-build/build/ckeditor';
+
 export default {
-  name: 'createThread'
-}
+  name: "createThread",
+  data() {
+    return {
+      title: '',
+      editor: Editor,
+      editorData: "<p>Describe your problem.</p>",
+      editorConfig: {
+        // The configuration of the editor.
+      },
+    };
+  },
+  methods: {
+    log( ) {
+      console.log(this.editorData)
+    }
+  },
+};
 </script>
--- vue/src/main.js
+++ vue/src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import { store } from './vuex/index.js'
+import CKEditor from '@ckeditor/ckeditor5-vue';
 
 const app = createApp(App)
 
 app
   .use(router)
   .use(store)
+  .use(CKEditor)
   .mount('#app')
